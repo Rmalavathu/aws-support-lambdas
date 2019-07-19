884afd03f0b8bfae22f31477f794735adb222458
Rmalavathu <44881314+Rmalavathu@users.noreply.github.com>
2019-07-19 14:19:04 -0400

    Update lambda_function_stop.py

--- Start and Stop/lambda_function_stop.py
+++ Start and Stop/lambda_function_stop.py
@@ -2,6 +2,11 @@ import boto3
 region = 'us-east-2'
 instances = []
 
+
+#Need a CloudWatch Role to trigger the lambda
+#Cron Expression for ClouldWatch Role 0 20 * * ? *
+#If you want to change the time change the second number (Uses 24 hour time)
+
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name=region)
     response = ec2.describe_instances()
