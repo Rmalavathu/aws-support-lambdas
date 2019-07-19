d2beb6170a64bcb671723d8f79bed2552c80a6f8
Rmalavathu <44881314+Rmalavathu@users.noreply.github.com>
2019-07-19 14:05:29 -0400

    Add files via upload

---
+++ Start and Stop/lambda_function_start.py
@@ -0,0 +1,16 @@
+import boto3
+region = 'us-east-2'
+instances = []
+
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2', region_name=region)
+    response = ec2.describe_instances()
+    for reservation in response["Reservations"]:
+        for instance in reservation["Instances"]:
+            temp = instance['Tags']
+            temp = temp[0]['Key']
+            tag = "Time"
+            if (temp == tag):
+                instances.append(instance["InstanceId"])
+    ec2.start_instances(InstanceIds=instances) 
+    
