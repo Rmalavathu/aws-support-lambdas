7404b899c2b4150af3d00005e106abc55ab57775
Rmalavathu <44881314+Rmalavathu@users.noreply.github.com>
2019-07-19 14:23:19 -0400

    Update lambda_function_dynamo.py

--- Status Check/lambda_function_dynamo.py
+++ Status Check/lambda_function_dynamo.py
@@ -1,9 +1,13 @@
 import json
 import boto3
-from pprint import pprint
 import time
 region = 'us-east-2'
 instances = ['i-009b1021b74fb9f21']
+
+#Work do this to account for both instances
+#Needs a CloudWatch Role for trigger
+#Cron Expression for CloudWatch Role 0/30 8-20 ? * * *
+#The first number is it updates every 30 minutes and 8-20 is the time interval when it updates
 
 def lambda_handler(event, context):
     client = boto3.client('ec2')
@@ -11,7 +15,6 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     dynamodbTable = dynamodb.Table('Daily30Checks')
     for i in response["InstanceStatuses"]:
-        print(time.ctime()[4])
         dynamodbTable.put_item(
             Item={
                 'Time':time.ctime(),
